769158847dea95bf1a67bc303f8054101a340f05
ChristopherGabba <chrisgabba2005@yahoo.com>
2026-07-26 15:46:43 -0500

    chore: upgrade Expo, React Native, and Nitro

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,12 +1,3 @@
-const path = require('path');
-const { getConfig } = require('react-native-builder-bob/babel-config');
-const pkg = require('../package.json');
-
-const root = path.resolve(__dirname, '..');
-
-module.exports = getConfig(
-  {
-    presets: ['babel-preset-expo'],
-  },
-  { root, pkg }
-);
+module.exports = {
+  presets: ['babel-preset-expo'],
+};
